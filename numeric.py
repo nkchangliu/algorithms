6ad4dfb5ebb69d4071a6dbc3b67ead811622bef9
chang <chang@changdeMacBook-Air.local>
2018-05-03 14:20:44 -0400

    fixed euclidean

--- numeric.py
+++ numeric.py
@@ -123,10 +123,13 @@ def reservoir_sample(lst, k):
 
 # euclidean meathod to find GCD
 def gcd(num1, num2):
-    if num1 == num2:
-        return num1
-    elif num1 > num2:
-        return gcd(num1 - num2, num2)
+    if num1 < num2:
+        num1, num2 = num2, num1
+    reminder = num1 % num2
+    if reminder == 0:
+        return num2
     else:
-        return gcd(num1, num2 - num1)
+        return gcd(num2, reminder)
+
+
 
